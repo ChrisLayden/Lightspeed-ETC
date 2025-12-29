f37f069d8e4c7f8021f31967e013bdc4f2534b1b
Christopher Layden <clayden7@mit.edu>
2025-12-29 12:02:12 -0500

    ETC website

--- scripts/website_etc.py
+++ scripts/website_etc.py
@@ -506,4 +506,5 @@ async def plot_mag_vs_precision(request: MagPrecisionPlotRequest):
 # ==================== Main ====================
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="http://hypernova.mit.edu", port=8080)
